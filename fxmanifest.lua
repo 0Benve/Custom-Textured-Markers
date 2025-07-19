153e6ed096960cfab3289562e77b23eb13d7653c
0Benve <benvescript@gmail.com>
2025-07-19 16:33:50 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+
+author '0Benve'
+description 'Custom Textured Markers, benve'
+version '1.0.0'
+
+client_script {
+  'client.lua'
+}
+
+data_file 'TEXTURE_DICTIONARY' 'stream/marker.ytd'
